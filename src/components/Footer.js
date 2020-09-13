792dd56e0e6e4fb0d20b04ac9de494e78ef21dbc
BuildTools <unconfigured@null.spigotmc.org>
2020-09-13 13:43:35 -0400

    remove footer branding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer(){
     return(
         <footer className="footer">
             <div className="content has-text-centered">
                 <p>
-                    &copy;2020 <strong>Market Ready Painting</strong> by <a href="https://coltonsweeney.com" rel="noopener noreferrer">Colton Sweeney</a>. 
+                    &copy;2020 <strong>Market Ready Painting</strong> <a href="https://www.facebook.com/MarketReadyPaintingNC" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="social" icon={faFacebook}/></a>
                 </p>
             </div>
         </footer>
